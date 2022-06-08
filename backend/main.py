1a24638e77ac3fa76b1e2b0c275904dc61ceddd8
ryuichi <ryuichi19633811@gmail.com>
2022-06-09 06:35:16 +0900

    add: useSWRでデータ取得

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["application/json"],
+)
 
 @app.get("/")
 def index():
---
+++ frontend/src/pages/Layout/Layout.tsx
@@ -0,0 +1,31 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import { ReactNode } from "react";
+
+export type Props ={
+    siteName: string;
+    desc: string;
+    children: ReactNode;
+}
+
+const Layout: NextPage<Props> = ({siteName, desc, children}) => {
+    return (
+        <>
+            <Head>
+                <title>{siteName}</title>
+                <meta name="description" content={desc} />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            </Head>
+
+            <main>
+                {children}
+            </main>
+
+            <footer>
+                <p>&copy; 2022 Dr.Brown All Rights Reserved.</p>
+            </footer>
+        </>
+    );
+}
+
+export default Layout;
