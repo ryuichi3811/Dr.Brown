006c502fe6521c02cc37f8784b86bf2260189837
ryuichi <ryuichi19633811@gmail.com>
2022-06-10 16:35:28 +0900

    create: ブラウンチェックのページ作成

---
+++ frontend/src/pages/BrownCheck.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Layout from './Layout/Layout'
+import Link from 'next/link'
+import Nav from '../components/nav/Nav'
+
+const siteName = "ブラウンチェック"
+const desc = "ミッキーを撮影し、画像判定結果を出すところ。"
+
+const BrownCheck: NextPage = () => {
+  return (
+    <Layout siteName={siteName} desc={desc}>
+      <Nav/>
+      <h1>{siteName}</h1>
+      <div><Link href={"Auth/Login"}>ログイン</Link></div>
+    </Layout>
+  )
+}
+
+export default BrownCheck
