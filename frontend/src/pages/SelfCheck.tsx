e2dee6166d395eb69fa8f6dbe569024b7ba2e5ea
ryuichi <ryuichi19633811@gmail.com>
2022-06-10 16:51:14 +0900

    fix: コード整理

--- frontend/src/pages/SelfCheck.tsx
+++ frontend/src/pages/SelfCheck.tsx
@@ -1,19 +1,21 @@
-import type { NextPage } from 'next'
-import Layout from './Layout/Layout'
-import Link from 'next/link'
-import Nav from '../components/nav/Nav'
+import type { NextPage } from "next";
+import Layout from "./Layout/Layout";
+import Link from "next/link";
+import Nav from "../components/nav/Nav";
 
-const siteName = "セルフチェック"
-const desc = "問診機能のページ"
+const siteName = "セルフチェック";
+const desc = "問診機能のページ";
 
 const SelfCheck: NextPage = () => {
   return (
     <Layout siteName={siteName} desc={desc}>
-      <Nav/>
+      <Nav />
       <h1>{siteName}</h1>
-      <div><Link href={"Auth/Login"}>ログイン</Link></div>
+      <div>
+        <Link href={"Auth/Login"}>ログイン</Link>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default SelfCheck
+export default SelfCheck;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,31 +1,33 @@
-import type { NextPage } from 'next'
-import Layout from './Layout/Layout'
-import axios from 'axios'
-import useSWR from 'swr'
-import Link from 'next/link'
-import Nav from '../components/nav/Nav'
+import type { NextPage } from "next";
+import Layout from "./Layout/Layout";
+import axios from "axios";
+import useSWR from "swr";
+import Link from "next/link";
+import Nav from "../components/nav/Nav";
 
 const siteName = "トップ";
 const desc = "トップページ";
 
 const url = "http://localhost:8000";
 
-const fetcher = (url: string) => axios.get(url).then(res => res.data);
+const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 const Home: NextPage = () => {
   // データ取得
-  const { data, error } = useSWR(url, fetcher)
-  if (error) return <div>読み込みに失敗しました。</div>
-  if (!data) return <div>少々お待ちください。。。</div>
+  const { data, error } = useSWR(url, fetcher);
+  if (error) return <div>読み込みに失敗しました。</div>;
+  if (!data) return <div>少々お待ちください。。。</div>;
   // console.log(data)
   return (
     <Layout siteName={siteName} desc={desc}>
-      <Nav/>
+      <Nav />
       <h1>{siteName}</h1>
-      <div><Link href={"Auth/Login"}>ログイン</Link></div>
+      <div>
+        <Link href={"Auth/Login"}>ログイン</Link>
+      </div>
       <h3>{data.test}</h3>
     </Layout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
