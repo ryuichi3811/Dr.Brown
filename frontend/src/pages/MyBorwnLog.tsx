e9cf7010a3e155fa9ad56a95659accdd86f6f0fa
ryuichi <ryuichi19633811@gmail.com>
2022-06-10 16:50:22 +0900

    create: 過去の記録ページを作成

---
+++ frontend/src/pages/MyBorwnLog.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from "next";
+import Layout from "./Layout/Layout";
+import Link from "next/link";
+import Nav from "../components/nav/Nav";
+
+const siteName = "過去の記録";
+const desc = "ミッキーの今までの結果を一覧にしてるページ";
+
+// ユーザーのログインのアカウントに飛ばしてから表示されるページなので、
+// lacalhost:3000/uuid/MyBrownLog
+// なお、これだけでなくメインページはおもにこの流れです。
+
+const MyBorwnLog: NextPage = () => {
+  return (
+    <Layout siteName={siteName} desc={desc}>
+      <Nav />
+      <h1>{siteName}</h1>
+      <div>
+        <Link href={"Auth/Login"}>ログイン</Link>
+      </div>
+    </Layout>
+  );
+};
+
+export default MyBorwnLog;
